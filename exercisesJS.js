f69b13d7370d5fc408e7393bff75ab6aaa7d83d4
Иван <kleimenov416@gmail.com>
2020-09-13 12:42:17 -0400

    add another puzzle

--- exercisesJS.js
+++ exercisesJS.js
@@ -1,19 +1,19 @@
-/*
+/* Puzzle #1
 In this exercise, we will be given array of 2 or more numbers.
 We will then have to find the two largest numbers in that
 array, and sum the together
 */
 
 let arr = [10, 4, 34, 6, 92, 2];
-
+[4,10,6,34]
 //I will use insertion sort approach for sorting array and when array will be sorted,
 //I just add last two elemets to each other 
 const sumLargestNumbers = function(data) {
     for (let i = 0; i < data.length; i++) {
-        let tmp = data[i]; 
+        let tmp = data[i];
         let previousIndex = i - 1; 
         while (previousIndex >= 0 && data[previousIndex] > tmp) {
-            data[previousIndex + 1] = data[previousIndex];
+            data[previousIndex + 1] = data[previousIndex]; 
             previousIndex-- ;
         }
         data[previousIndex + 1] = tmp;
@@ -52,3 +52,32 @@ let sortedArray = function(data) {
     }
     return data;
 }
+
+/* Puzzle #2
+For this puzzle, we will be adding only the numbers in the array which match the given condition
+*/
+
+
+var conditonaSum = function(values, condition) {
+    var evenNumbers = [];
+    var oddNumbers = [];
+    for (var i = 0; i < values.length; i++) {
+      if(values[i] % 2 === 0) {
+        evenNumbers.push(values[i]);
+      }
+      else if (values[i] %2 !== 0) {
+        oddNumbers.push(values[i]);
+      }
+    }
+    if (condition === 'even') {
+      return evenNumbers.reduce(function(acc, val) { return acc + val; }, 0);
+    }
+    else {
+      return oddNumbers.reduce(function(acc, val) { return acc + val; }, 0);
+    }
+  }
+  
+  
+  var values = [1,2,3,4,5];
+  var condition = 'even';
+  console.log(conditonaSum(values, condition))
