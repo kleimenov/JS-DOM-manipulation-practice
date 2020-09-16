7de99661606409e2f74bed3ebea4d1fb241ceb74
Иван <kleimenov416@gmail.com>
2020-09-16 12:59:58 -0400

    add solution for exercise 1

--- exerciseWithNode.js
+++ exerciseWithNode.js
@@ -1,3 +1,4 @@
+/*
 let arr = ['clean', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'];
 let threshold = 0.5;
 
@@ -13,3 +14,33 @@ const checkAir = function (samples, threshold) {
   return airPollution < threshold ? "Clean" : "Polluted";
 }
 console.log(checkAir(arr, threshold))
+*/
+
+//Exercise #1
+//Write a function that will count each character in string and return object with result
+
+let stringNew = "I haave a cat, my cat is super cat and hi know IT!!!!"
+
+let charCounter = function(str){
+    // make an object to return at the and
+    let result = {};
+    // loop over string, for each character
+    for(let i = 0; i < str.length; i++) {
+        let char = str[i];
+        //if char is a number/letter and ia a key in object, add one to count
+        if(result[char] > 0) {
+            result[char]++;
+        }
+        //if the char is a number/letter and not in object, add it tio onbecct and set value to 1
+        else {
+            result[char] = 1;
+        };
+    }
+    //if character is somethinv else (space, period, etc.) don't do anything
+    //return obhect at the end
+    return result;
+}
+
+console.log(charCounter(stringNew))
+
+
