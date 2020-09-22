39a1daea99c34588c39b11129b5a4f6c48845cee
Иван <kleimenov416@gmail.com>
2020-09-22 12:49:34 -0400

    change solution

--- minValue.js
+++ minValue.js
@@ -3,9 +3,7 @@ let array = [2, -1, 4 , 6 , 9]
 const minimumValue = function(arr){
     let minValue = 0;
     for(let value = 0; value < arr.length; value++){
-        if(arr[value] < minValue) {
-            minValue = arr[value];
-        }
+            minValue = arr[value] < minValue ? arr[value]: minValue;
     }
     return minValue;
 }
