130997789fab4e056e590ca44e8bf76d9731e717
Иван <kleimenov416@gmail.com>
2020-09-23 14:01:58 -0400

    add file iterating-over-objectII.js and explore how to iterate through objects

--- iterating-over-objectII.js
+++ iterating-over-objectII.js
@@ -3,10 +3,11 @@ const person = {
   lastName: 'Glueman',
   weightInKg: 73,
   heightInCm: 173,
-  numbers: [2, 3, 98]
+  numbers: [2, 3, 98],
+  arrayOfObject: [{firstNumber:666, secondNumber: 777, lastNumber: 000,}]
   };
   
-
+/*
 for (let keyName in person) {
     if (keyName === 'numbers') {
       for (let numbers of person[keyName]) {
@@ -14,9 +15,28 @@ for (let keyName in person) {
     }
   }
 }
+*/
+for(let keys in person) {
+  if (keys === 'arrayOfObject') {
+    person[keys].forEach(function(value) {
+      console.table(value)
+      for (let nestValue in value) {
+        console.log(value[nestValue])
+      }
+    })
+  }
+}
+
 
 
 
 //console.log(Object.values(person));
 //console.log(Object.keys(person));
 //console.log(Object.keys(person), Object.values(person));
+
+
+// bonus thing is console.table()
+
+let array = [12,58,99,89,74,81]
+
+console.table(array);
