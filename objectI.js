6dcc0bb1294b4329c5139657c73c7bc1b340a60f
Иван <kleimenov416@gmail.com>
2020-09-23 11:23:06 -0400

    add file objectI.js, in this file i will discover object data type properties

---
+++ objectI.js
@@ -0,0 +1,17 @@
+
+
+const person = {
+  name: 'Ivan',
+  lastName: 'Glueman',
+  fullName: function() {
+      console.log(`${this.name} ${this.lastName}`);
+  },
+  weightInKg: 73,
+  heightInCm: 173,
+  heightInInches: function(heightInCm) {
+    console.log(Math.floor(`${this.heightInCm}` / 2.54));
+    }
+};
+
+person.fullName();
+person.heightInInches();
